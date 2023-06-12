86f1d356701d766f2ed9fdbbdef5debb214a9110
Souuum <hugo.soumbrugevin@gmail.com>
2023-06-12 20:19:50 +0200

    [add] basic Patient population

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,6 @@
-// import db from "./index"
+import db from "./index"
+import { fakerFR as faker } from "@faker-js/faker"
+import { SecurePassword } from "@blitzjs/auth/secure-password"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -10,6 +12,43 @@ const seed = async () => {
   // for (let i = 0; i < 5; i++) {
   //   await db.project.create({ data: { name: "Project " + i } })
   // }
+
+  for (let i = 0; i < 30; i++) {
+    // patients creation
+    const gender = faker.person.sexType()
+    const firstName = faker.person.firstName(gender)
+    const lastName = faker.person.lastName()
+    const email = faker.internet.email({ firstName, lastName })
+    const securityNumber = faker.string.numeric({ length: 13 })
+    const password = faker.internet.password({ length: 10 })
+    const hashedPassword = await SecurePassword.hash(password.trim())
+    const phone = faker.phone.number()
+    const birthDate = faker.date.birthdate({ min: 18, mode: "age" })
+
+    const usr = await db.user.create({
+      data: {
+        gender: gender,
+        firstName: firstName,
+        name: lastName,
+        email: email,
+        securityNumber: securityNumber,
+        hashedPassword: hashedPassword,
+        phone: phone,
+        birthDate: birthDate,
+      },
+    })
+
+    const patient = await db.patient.create({
+      data: {
+        gender: gender,
+        firstName: firstName,
+        name: lastName,
+        userId: usr.id,
+      },
+    })
+
+    console.log(patient)
+  }
 }
 
 export default seed
