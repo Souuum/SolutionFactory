a21ed61a31304121f62b7e05dffb6875aeb8786b
Souuum <hugo.soumbrugevin@gmail.com>
2023-06-22 09:05:05 +0200

    modified patientTable

--- src/patient/components/PatientTable.tsx
+++ src/patient/components/PatientTable.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router"
 import { Link } from "next/link"
 import getAllPatients from "../queries/getAllPatients"
 
-const ITEMS_PER_PAGE = 5
+const ITEMS_PER_PAGE = 6
 
 export const PatientTable = () => {
   const router = useRouter()
@@ -18,7 +18,25 @@ export const PatientTable = () => {
   const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
   const goToNextPage = () => router.push({ query: { page: page + 1 } })
   return (
-    <div>
+    <div className="container max-w-3xl px-4 mx-auto sm:px-8">
+      <div className="text-end">
+        <form className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
+          <div className=" relative ">
+            <input
+              type="text"
+              id='"form-subscribe-Filter'
+              className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+              placeholder="name"
+            />
+          </div>
+          <button
+            className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+            type="submit"
+          >
+            Filter
+          </button>
+        </form>
+      </div>
       <table className="min-w-full leading-normal">
         {/* Table header */}
         <thead>
